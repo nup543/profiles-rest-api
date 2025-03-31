98b441f279e6c4743a781129264fc75ee6298d86
nup543 <nups543@gmail.com>
2025-03-31 16:38:01 +0530

    configured for deployement

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k_$*puehxkf=i-hg+jou-6%9*&d7m6$t0^ilj&$kz&9(i8a27w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.User'
+
+STATIC_ROOT = 'static/'
